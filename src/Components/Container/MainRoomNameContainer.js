285738546764603510a4ed1afae1c4ab7f004e35
zktm9903 <zktm9903@naver.com>
2022-01-04 14:56:58 +0900

    ✅  친구 초대 기능 추가

--- src/Components/Container/MainRoomNameContainer.js
+++ src/Components/Container/MainRoomNameContainer.js
@@ -2,6 +2,8 @@ import MainRoomNamePresenter from "../Presenter/MainRoomNamePresenter";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useState } from "react";
+import InviteRoom from "./InviteRoom";
 
 const RoomName = styled.p`
     color:white;
@@ -11,11 +13,24 @@ const RoomName = styled.p`
 
 const MainRoomNameContainer = ({ selectRoom }) => {
     const store = useSelector(state => state);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+
+    const [InputId, setInputId] = useState('');
+
+    const InvitePerson = () => {
+        console.log(InputId)
+    }
+
+    const changeInputId = (e) => {
+        setInputId(e.target.value)
+    }
 
     return (
         <MainRoomNamePresenter>
             <RoomName>{selectRoom !== 0 && store.rooms[selectRoom - 1].name}</RoomName>
-            {selectRoom !== 0 && <AiOutlinePlusCircle />}
+            {selectRoom !== 0 && <AiOutlinePlusCircle onClick={() => setShow(true)} />}
+            <InviteRoom set={show} setShow={handleClose} InvitePerson={InvitePerson} changeInputId={changeInputId} />
         </MainRoomNamePresenter>
     )
 }
